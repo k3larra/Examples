8e5b97af40df734e792f6d69a5bef41535d24677
Lars <k3larra@hotmail.com>
2013-03-14 11:53:00 +0100

    Thread example

---
+++ ThreadTest/src/se/mah/k3/TestGUI.java
@@ -0,0 +1,62 @@
+package se.mah.k3;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.SystemColor;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class TestGUI extends JFrame {
+
+	private JPanel contentPane;
+	private JLabel lblNewLabel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TestGUI frame = new TestGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TestGUI() {
+		//Skapa vår uppgift där anropet till nätet skall ligga det som kommmer att köras i en annan tråd
+		//Vi skickar med this som är en referens till oss själva så att tråden kan skicka tillbaka info till oss.
+		TaskRunner task  = new TaskRunner(this,1000);
+		//Skapa en tråd att stoppa in uppgiften i
+		Thread t = new Thread(task);
+		//starta tråden
+		t.start(); 
+		//Det vanliga krafset
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		lblNewLabel = new JLabel("Time now");
+		contentPane.add(lblNewLabel, BorderLayout.NORTH);
+	}
+	
+	//Här är metoden som tråden anropar nu får vi en sträng tillbaka men här kan vi ta emot vad som helst
+	public void SomethingHappend(String s){
+		System.out.println(s);
+		lblNewLabel.setText(s);
+	}
+	
+
+}
